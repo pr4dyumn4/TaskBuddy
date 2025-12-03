2cd70314b26b471fa4ab46a9f93385b4a94361d0
pr4dyumn4 <pradyumnajekumar@gmail.com>
2025-12-03 11:26:39 +0530

    added vercel Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import TaskList from "./TaskList";
 import './index.css'
 import './App.css'
+import { Analytics } from "@vercel/analytics/next"
 function App() {
   return (
     <div>
       <TaskList/>
+      <Analytics />
     </div>
   );
 }
